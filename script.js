87e41f3006c30dc1e2b494b570b6459ef8a70fd3
Adrienne Mok <adriennemok@users.noreply.github.com>
2018-05-14 12:26:35 -0700

    adriennemok.github.io

--- script.js
+++ script.js
@@ -2,39 +2,29 @@
 /*global $ */
 
 $(document).ready(function () {
-    "use strict";    
-    // Text Fader
-//    $('.flyer>span:not(:first-child)').hide();
-//    var count = 1;
-//    setInterval(function () {
-//        var objs = $('.flyer>span');
-//        objs.hide();
-//        $(objs[count % objs.length]).fadeIn();
-//        count++;
-//    }, 2000);
-        
-var quotes = $(".delayedText");
-var quoteIndex = -1;
+    "use strict";            
+    var quotes = $(".delayedText");
+    var quoteIndex = -1;
 
-function showNextQuote() {
-    ++quoteIndex;
-    quotes.eq(quoteIndex % quotes.length)
-        .fadeIn(1000)
-        .delay(1000)
-        .fadeOut(1000, showNextQuote);
-}
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(1000)
+            .delay(1000)
+            .fadeOut(1000, showNextQuote);
+    }
 
-showNextQuote();
-    //smooth scrolling
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        var target = this.hash;
-        var $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 900, 'swing', function () {
-            window.location.hash = target;
+    showNextQuote();
+        //smooth scrolling
+        $('a[href^="#"]').on('click', function (e) {
+            e.preventDefault();
+            var target = this.hash;
+            var $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 900, 'swing', function () {
+                window.location.hash = target;
+            });
         });
-    });
 });
 
